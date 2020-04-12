19c237b73dda10154acd446835298effe7c30e0f
Huang Chi-Heng <huangchi-heng@Huangs-MacBook-Pro.local>
2020-04-12 23:24:44 +0800

    update last check to search string

--- cypress/integration/Booking-exmaple.js
+++ cypress/integration/Booking-exmaple.js
@@ -1,35 +1,36 @@
-describe('The Expedia Home Page', function() {
+describe("The Expedia Home Page", function () {
   // beforeEach(() => {
   //   cy.visit('https://www.agoda.com/?pv=0');
   // });
 
-  it('.type() - type into a DOM element', () => {
+  it(".type() - type into a DOM element", () => {
     cy.server();
-    cy.route('POST', '**/get_handpicked_bh_properties').as('search-request');
-    cy.route('POST', '**/rack_rates/rr_log_rendered').as('submit-booking');
+    cy.route("POST", "**/get_handpicked_bh_properties").as("search-request");
+    cy.route("POST", "**/rack_rates/rr_log_rendered").as("submit-booking");
 
-    cy.visit('https://www.booking.com/index.html');
+    cy.visit("https://www.booking.com/index.html");
 
-    cy.get('input[name=ss]')
-      .type('tpe')
-      .should('have.value', 'tpe');
+    cy.get("input[name=ss]").type("tpe").should("have.value", "tpe");
 
-    cy.wait('@search-request');
+    cy.wait("@search-request");
     cy.get('[data-label="Taipei, Taipei Area, Taiwan"]').click();
 
-    cy.get('.bui-calendar').should('be.visible');
+    cy.get(".bui-calendar").should("be.visible");
 
-    cy.get('td[data-date=2019-12-28]').click();
-    cy.get('td[data-date=2019-12-29]').click();
+    cy.get("td[data-date=2020-05-28]").click();
+    cy.get("td[data-date=2020-05-29]").click();
 
-    cy.get('[data-placeholder="Check-in"]').should('have.text', 'Sat, Dec 28');
-    cy.get('[data-placeholder="Check-out"]').should('have.text', 'Sun, Dec 29');
+    cy.get('[data-placeholder="Check-in"]').should("have.text", "Thu, May 28");
+    cy.get('[data-placeholder="Check-out"]').should("have.text", "Fri, May 29");
     // cy.get('.DayPicker-Month').should('contain', 'December 2019');
 
     cy.get('button[data-sb-id="main"]').click();
 
-    cy.wait('@submit-booking');
+    cy.wait("@submit-booking");
 
-    cy.location('pathname').should('include', '/searchresults.en-us');
+    cy.location("search").should(
+      "include",
+      "checkin_year=2020&checkin_month=5&checkin_monthday=28&checkout_year=2020&checkout_month=5&checkout_monthday=29"
+    );
   });
 });
